94708f5b1ae42aab2a51c9afb8ef66465900ecc9
lakshanjayalath <iamdhananjaya119@gmail.com>
2025-09-23 13:28:41 +0530

    Implement basic Express server with message sending functionality and configuration setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { response } from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import { APP_CONFIG } from "./config/app.config";
+import { MessageController } from "./controller/message.controller";
+
+
+const app = express();
+
+app.use(express.json());
+
+const messageController = new MessageController();
+
+app.post("/send-message", messageController.sendMessage);
+
+
+app.listen(8558, () => {
+    console.log("Server is running on port 8558");
+});
+
